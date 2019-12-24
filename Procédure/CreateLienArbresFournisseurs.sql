daa100d99e6a789be01591b7eca76b0b00075077
smits <victor>
2019-12-24 11:08:20 +0100

    fixing procedure

--- Procédure/CreateLienArbresFournisseurs.sql
+++ Procédure/CreateLienArbresFournisseurs.sql
@@ -1,11 +1,11 @@
-CREATE PROCEDURE `CreateLienArbreFournisseur` (PK_Arbre int(11), PK_Fournisseur int(11), prix int(11), nb(11))
+CREATE PROCEDURE `CreateLienArbreFournisseur` (PK_Arbre int(11), PK_Fournisseur int(11), prix int(11), nb int(11))
 BEGIN
 insert into LienArbresFournisseurs(FK_Arbre,FK_Fournisseurs,Prix,Nb) 
-values(PK_Arbre,PK_Fournisseurs,prix,,nb)
+values(PK_Arbre,PK_Fournisseurs,prix,nb);
 Select
 PK_LienArbresFournisseurs
 From
 LienArbresFournisseurs
 where
-LienArbresFournisseurs.FK_Arbre = PK_Arbre and LienArbresFournisseurs.FK_Fournisseurs = PK_Fournisseur
+LienArbresFournisseurs.FK_Arbre = PK_Arbre and LienArbresFournisseurs.FK_Fournisseurs = PK_Fournisseur;
 END
